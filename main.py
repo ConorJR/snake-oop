2b52c2b03cc2c3554b59a141c8ff7c3355be4145
Maryja Haršunova <118957324+dziauco@users.noreply.github.com>
2024-02-18 16:04:59 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 from turtle import *
-
-
-t1 = Turtle()
-t2 = Turtle()
-t3 = Turtle()
+hideturtle()
+speed(10)
+t1= Turtle()
+t2= Turtle()
+t3=Turtle()
 
 t1.color('red')
 t2.color('green')
 t3.color('blue')
-t2.left(120)
-t3.left(240)
+t2.left(162)
+t3.left(270)
 
-for i in range(36):
+for i in range(28):
     t1.forward(10)
     t1.left(10)
 
@@ -21,3 +21,4 @@ for i in range(36):
     t3.forward(10)
     t3.left(10)
 
+exitonclick()
